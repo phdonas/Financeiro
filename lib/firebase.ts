0073b51970b16fce37b7aaf58639475ac6ff834b
phdonas <89523419+phdonas@users.noreply.github.com>
2026-01-01 21:26:32 +0000

    Set up Firebase authentication and Firestore

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,4 +1,17 @@
-// Versão Local: Firebase desativado
-export const auth = null;
-export const db = null;
-export default null;
+import { initializeApp } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCjE2Mrt92wpJR8q2g0sl54gNcWNJe4us0",
+  authDomain: "phdgesfin.firebaseapp.com",
+  projectId: "phdgesfin",
+  storageBucket: "phdgesfin.firebasestorage.app",
+  messagingSenderId: "506094815396",
+  appId: "1:506094815396:web:ace0b80e7ff54f92f53e31"
+};
+
+export const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const googleProvider = new GoogleAuthProvider();
